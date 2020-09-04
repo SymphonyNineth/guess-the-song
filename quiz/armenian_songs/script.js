68e95866e75201569dd80dc0986e5afde33240dc
Hayk <webdevfix@gmail.com>
2020-09-04 12:41:39 +0400

    Added Armenian songs

---
+++ quiz/armenian_songs/script.js
@@ -0,0 +1,366 @@
+let songs = [
+    {
+        title: "Արթուր Մեսչյան - Ես չգիտեմ",
+        src: "../../audio/armenian_songs/arthur_meschyan/es_chgitem.mp3",
+        options: [
+            {name: "Արթուր Մեսչյան - Ես չգիտեմ", correct: true},
+            {name: "Dorians - Ես Կուլամ", correct: false},
+            {name: "Project LA - Փնտրում ես դու", correct: false},
+            {name: "Լավ էլի - Գարուն է եկել", correct: false}
+        ]
+    },
+
+    
+
+    {
+        title: "Dorians - Ես Կուլամ",
+        src: "../../audio/armenian_songs/dorians/es_kulam.mp3",
+        options: [
+            {name: "Արթուր Մեսչյան - Ես չգիտեմ", correct: false},
+            {name: "Dorians - Ես Կուլամ", correct: true},
+            {name: "Վահագ Ռաշ - Հեռացիր", correct: false},
+            {name: "Լավ էլի - Ճամփորդական երգ", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Գռորգի Մինասյան - Սիրո հասակ",
+        src: "../../audio/armenian_songs/georgi_minasyan/siro_hasak.mp3",
+        options: [
+            {name: "Լավ Էլի - Գտա քեզ երկնքում", correct: false},
+            {name: "Project LA - Ահա նորից գիշեր", correct: false},
+            {name: "Project LA - Ով կհանդիպի", correct: false},
+            {name: "Գռորգի Մինասյան - Սիրո հասակ", correct: true}
+        ]
+    },
+
+    
+    {
+        title: "Լավ էլի - Ճամփորդական երգ",
+        src: "../../audio/armenian_songs/lav_eli/champordakan_erg.mp3",
+        options: [
+            {name: "Project LA - Խաղաղ գիշերով", correct: false},
+            {name: "Dorians - Ես Կուլամ", correct: false},
+            {name: "Լավ էլի - Ճամփորդական երգ", correct: true},
+            {name: "Լավ էլի - Գարուն է եկել", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Լավ էլի - Գարուն է եկել",
+        src: "../../audio/armenian_songs/lav_eli/garun_e_ekel.mp3",
+        options: [
+            {name: "The beautified project - Արագիլ", correct: false},
+            {name: "Լավ էլի - Գարուն է եկել", correct: true},
+            {name: "Վահագ Ռաշ - Ես կգամ", correct: false},
+            {name: "Project LA - Փնտրում ես դու", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Լավ Էլի - Գտա քեզ երկնքում",
+        src: "../../audio/armenian_songs/lav_eli/gta_qez_erknqum.mp3",
+        options: [
+            {name: "Ռուբեն Հախվերդյան - Եթե իմանայիր", correct: false},
+            {name: "Լավ Էլի - Գտա քեզ երկնքում", correct: true},
+            {name: "Արթուր Մեսչյան - Ես չգիտեմւ", correct: false},
+            {name: "Լավ էլի - Գարուն է եկել", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Լիլիթ Պիպոյան - Մի լար",
+        src: "../../audio/armenian_songs/lilit_pipoyan/mi_lar.mp3",
+        options: [
+            {name: "Լիլիթ Պիպոյան - Մի լար", correct: true},
+            {name: "Dorians - Ես Կուլամ", correct: false},
+            {name: "Ռուբեն Հախվերդյան - Եթե իմանայիր", correct: false},
+            {name: "Լավ էլի - Գարուն է եկել", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Project LA - Ահա նորից գիշեր",
+        src: "../../audio/armenian_songs/project_la/aha_noric_gisher.mp3",
+        options: [
+            {name: "Project LA - Խաղաղ գիշերով", correct: false},
+            {name: "The beautified project - Կիլիկիա", correct: false},
+            {name: "Project LA - Ահա նորից գիշեր", correct: true},
+            {name: "Գռորգի Մինասյան - Սիրո հասակ", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Project LA  - Երկու ուևվական",
+        src: "../../audio/armenian_songs/project_la/erku_urvakan.mp3",
+        options: [
+            {name: "Project LA  - Երկու ուևվական", correct: true},
+            {name: "Լավ էլի - Գտա քեզ երկնքում", correct: false},
+            {name: "Լավ էլի - Գարուն է եկել", correct: false},
+            {name: "Project LA - Մահվան տեսիլ", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Project LA  - Մահվան տեսիլ",
+        src: "../../audio/armenian_songs/project_la/mahvan_tesil.mp3",
+        options: [
+            {name: "Project LA  - Մահվան տեսիլ", correct: true},
+            {name: "Արթուր Մեսչյան - Ես չգիտեմ", correct: false},
+            {name: "Project LA - Ահա նորից գիշեր", correct: false},
+            {name: "Վահագ Ռաշ - Հեռացիր", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Project LA - Ով կհանդիպի",
+        src: "../../audio/armenian_songs/project_la/ov_khandipi.mp3",
+        options: [
+            {name: "Լավ էլի - Ճամփորդական երգ", correct: false},
+            {name: "Project LA - Ես Փնտրում ես դու", correct: false},
+            {name: "Dorians - Ես Կուլամ", correct: false},
+            {name: "Project LA - Ով կհանդիպի", correct: true}
+        ]
+    },
+
+    
+    {
+        title: "Project LA - Փնտրում ես դու",
+        src: "../../audio/armenian_songs/project_la/pntrum_es_du.mp3",
+        options: [
+            {name: "Լավ էլի - Գարուն է եկել", correct: false},
+            {name: "Dorians - Ես Կուլամ", correct: false},
+            {name: "Project LA - Փնտրում ես դու", correct: true},
+            {name: "Ռուբեն Հախվերդյան - Գտնված երազ", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Project LA - Խաղաղ գիշերով",
+        src: "../../audio/armenian_songs/project_la/xaxax_gisherov.mp3",
+        options: [
+            {name: "Ռուբեն Հախվերդյան - Շատախոս անձրևը", correct: false},
+            {name: "Ռուբեն Հախվերդյան - Ծեր գնչուհին", correct: false},
+            {name: "Վահագ Ռաշ - Ես կգամ", correct: false},
+            {name: "Project LA - Խաղաղ գիշերով", correct: true}
+        ]
+    },
+
+    
+    {
+        title: "Ռուբեն Հախվերդյան - Ծեր գնչուհին",
+        src: "../../audio/armenian_songs/ruben_haxverdyan/cer_gnchuhin.mp3",
+        options: [
+            {name: "Ռուբեն Հախվերդյան - Ծեր գնչուհին", correct: true},
+            {name: "Լավ էլի - Գտա քեզ երկնքում", correct: false},
+            {name: "Project LA - Ով կհանդիպի", correct: false},
+            {name: "Լավ էլի - Գարուն է եկել", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Ռուբեն Հախվերդյան - Եթե իմանայիր",
+        src: "../../audio/armenian_songs/ruben_haxverdyan/ete_imanair.mp3",
+        options: [
+            {name: "Արթուր Մեսչյան - Ես չգիտեմ", correct: false},
+            {name: "Ռուբեն Հախվերդյան - Եթե իմանայիր", correct: true},
+            {name: "Լիլիթ Պիպոյան - Մի լար", correct: false},
+            {name: "Ռուբեն Հախվերդյան - Մեր սիրո աշունը", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Ռուբեն Հախվերդյան - Գտնված երազ",
+        src: "../../audio/armenian_songs/ruben_haxverdyan/gtnvac_eraz.mp3",
+        options: [
+            {name: "Dorians - Ես կուլամ", correct: false},
+            {name: "Ռուբեն Հախվերդյան - Գտնված երազ", correct: true},
+            {name: "Project LA - Փնտրում ես դու", correct: false},
+            {name: "Լավ էլի - Գարուն է եկել", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Ռուբեն Հախվերդյան - Իմ փոքրիկ նավակ",
+        src: "../../audio/armenian_songs/ruben_haxverdyan/im_poqrik_navak.mp3",
+        options: [
+            {name: "Project LA - Ահա նորից գիշեր", correct: false},
+            {name: "Dorians - Ես Կուլամ", correct: false},
+            {name: "Project LA - Փնտրում ես դու", correct: false},
+            {name: "Ռուբեն Հախվերդյան - Իմ փոքրիկ նավակ", correct: true}
+        ]
+    },
+
+    
+    {
+        title: "Ռուբեն Հախվերդյան - Մեր սիրո աշունը",
+        src: "../../audio/armenian_songs/ruben_haxverdyan/mer_siro_ashuny.mp3",
+        options: [
+            {name: "Լավ էլի - Գարուն է եկել", correct: false},
+            {name: "Ռուբեն Հախվերդյան - Եթե իմանայիր", correct: false},
+            {name: "Ռուբեն Հախվերդյան - Մեր սիրո աշունը", correct: true},
+            {name: "Վահագ Ռաշ - Լուսամփոփի պես աղջիկ", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "The beautified project - Արագիլ",
+        src: "../../audio/armenian_songs//the_beautified_project/aragil.mp3",
+        options: [
+            {name: "The beautified project - Արագիլ", correct: true},
+            {name: "Project LA - Մահվան տեսիլ", correct: false},
+            {name: "Project LA - Փնտրում ես դու", correct: false},
+            {name: "Լավ էլի - Գտա քեզ երկնքում", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Ռուբեն Հախվերդյան - Շատախոս անձրևը",
+        src: "../../audio/armenian_songs/ruben_haxverdyan/shataxos_andzrevy.mp3",
+        options: [
+            {name: "Վահագ Ռաշ - ես Կգամ", correct: false},
+            {name: "Ռուբեն Հախվերդյան - Շատախոս անձրևը", correct: true},
+            {name: "Dorians - Ես կուլամ", correct: false},
+            {name: "Լավ էլի - Գարուն է եկել", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "The beautified project - Կիլիկիա",
+        src: "../../audio/armenian_songs/the_beautified_project/kilikia.mp3",
+        options: [
+            {name: "Լավ էլի - Ճամփորդական երգ", correct: false},
+            {name: "The beautified project - Արագիլ", correct: false},
+            {name: "Project LA - Մահվան տեսիլ", correct: false},
+            {name: "The beautified project - Կիլիկիա", correct: true}
+        ]
+    },
+
+    
+    {
+        title: "Վահագ Ռաշ - Անհաս մի տենչ",
+        src: "../../audio/armenian_songs/vahag_rush/anhas_mi_tench.mp3",
+        options: [
+            {name: "Վահագ Ռաշ - Ես կգամ", correct: false},
+            {name: "Արթուր Մեսչյան - Ես չգիտեմ", correct: false},
+            {name: "Վահագ Ռաշ - Անհաս մի տենչ", correct: true},
+            {name: "Project LA - Երկու ուրվական", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Վահագ Ռաշ - Ես կգամ",
+        src: "../../audio/armenian_songs/vahag_rush/es_kgam.mp3",
+        options: [
+            {name: "Վահագ Ռաշ - Ես կգամ", correct: true},
+            {name: "Լավ էլի - Գտա քեզ երկնքում", correct: false},
+            {name: "Գեորգի Մինասյան - Սիրո հասակ", correct: false},
+            {name: "Dorians - Ես Կուլամ", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Վահագ Ռաշ - Հեռացիր",
+        src: "../../audio/armenian_songs/vahag_rush/heracir.mp3",
+        options: [
+            {name: "Վահագ Ռաշ - Հեռացիր", correct: true},
+            {name: "Վահագ Ռաշ - Լուսամփոփի պես աղջիկ", correct: false},
+            {name: "Ռուբեն Հախվերդյան - Ծեր գնչուհին", correct: false},
+            {name: "Լավ էլի - Գարուն է եկել", correct: false}
+        ]
+    },
+
+    
+    {
+        title: "Վահագ Ռաշ - Լուսամփոփի պես աղջիկ",
+        src: "../../audio/armenian_songs/vahag_rush/lusampopi_pes_axjik.mp3",
+        options: [
+            {name: "Project LA - Ով կհանդիպի", correct: false},
+            {name: "Dorians - Ես Կուլամ", correct: false},
+            {name: "Project LA - Ես Փնտրում ես դու", correct: false},
+            {name: "Վահագ Ռաշ - Լուսամփոփի պես աղջիկ", correct: true}
+        ]
+    }
+];
+let playerContainer = document.querySelector(".playerContainer"),
+    player = document.querySelector(".player"),
+    grid = document.querySelector(".grid"),
+    next = document.querySelector(".next"),
+    finalPage = document.querySelector(".finish");
+    wrongCount = document.querySelector("#wrongCount");
+
+    let wrongAnswers = 0;
+
+let shuffledSongs = songs.sort(() => Math.random() - 0.5),
+    currentIndex = 0;
+
+    function start(){
+        player.src = shuffledSongs[0].src;
+        player.play();
+    }
+
+function showOptions(){
+    shuffledSongs[currentIndex].options.forEach((option) => {
+        const button = document.createElement("button");
+        button.innerText = option.name;
+        if(option.correct){
+            button.dataset.correct = true;
+        }
+        button.addEventListener("click", selectAnswer);
+                
+        grid.appendChild(button);
+    }); 
+}
+
+function eraseData(){
+    grid.innerHTML = "";
+    next.classList.add("hide");
+}
+function selectAnswer(e){
+    let button = e.target;
+    if(button.dataset.correct){
+        button.classList.add("correct");
+        currentIndex ++;
+        next.classList.remove("hide");
+    } else{
+        button.classList.add("incorrect");
+        wrongAnswers ++;
+    }
+}
+function nextSong(){
+    if(shuffledSongs.length > currentIndex + 1){
+        eraseData();
+        player.src = shuffledSongs[currentIndex].src;
+        showOptions();
+    } else {
+
+    }
+}
+
+function finish(){
+    wrongCount.innerText = wrongAnswers;
+    player.src = "../../audio/andantino.mp3";
+    playerContainer.classList.add("hide");
+    eraseData();
+    finalPage.classList.remove("hide");
+}
+window.onload = start;
+showOptions();
+next.addEventListener("click", nextSong);
